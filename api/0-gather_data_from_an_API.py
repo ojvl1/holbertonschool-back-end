c205f143ef2b7291b0ca08dd7acb5c0b30a03200
ojvl1 <antompietriosvaldo@gmail.com>
2024-04-17 23:13:09 -0400

    usinf rest api to retrive information

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,38 +1,43 @@
 #!/usr/bin/python3
 """API REST"""
 import requests
-import sys
+import sys, argv
 
 
-def fetch_employee_todo_list(employee_id):
-    """Function that retrive information from the API REST"""
-    API_info = f'https://jsonplaceholder.typicode.com/todos?userId={employee_id}'
-    response = requests.get(API_info)
-    if response.status_code != 200:
-        print("Error:", response.text)
-        return
+def get_employee(id=None):
+    """
+    Function that retrive information from the REST API
+    """
+    # check if argv[1] is a number int
+    if len(argv) > 1:
+        try:
+            id = int(argv[1])
+        except ValueError:
+            return
 
-    data = response.json()
-    # Assuming 'userId' contains the employee's name
-    employee_name = data[0]['userId']
+    if isinstance(id, int):
+        base = "https://jsonplaceholder.typicode.com"
+        user = requests.get(f"{base}/users/{id}").json()
+        to_dos = requests.get(f"{base}/todos/?userId={id}").json()
 
-    completed_tasks = []
-    for task in data:
-        if task['completed']:
-            completed_tasks.append(task['title'])
+        if user and to_dos:
+            total_tasks = len(to_dos)
+            # fmt: off
+            titles_completed = [task["title"]
+                                for task in to_dos
+                                if task["completed"]]
+            # fmt: on
+            tasks_completed = len(titles_completed)
 
-    total_tasks = len(data)
+            print(
+                "Employee {} is done with tasks({}/{}):".format(
+                    user["name"], tasks_completed, total_tasks
+                )
+            )
 
-    print(f"Employee {employee_name} is done with tasks(
-        {len(completed_tasks)}/{total_tasks}): ")
-    for task_title in completed_tasks:
-        print(f"    {task_title}")
+            for title in titles_completed:
+                print(f"\t {title}")
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python3 script.py <employee_id>")
-        sys.exit(1)
-
-    employee_id = int(sys.argv[1])
-    fetch_employee_todo_list(employee_id)
+    get_employee()
