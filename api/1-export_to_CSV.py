1a2db6d4190fe4d27f4de304cda611531e533467
ojvl1 <antompietriosvaldo@gmail.com>
2024-04-18 11:38:26 -0400

    export to csv

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,47 +1,41 @@
 #!/usr/bin/python3
+"""Script to use a REST API for a given employee ID, returns
+information about his/her TODO list progress"""
 import requests
-import csv
 import sys
+import csv
 
-def fetch_employee_todo_list(employee_id):
-    API_info = f'https://jsonplaceholder.typicode.com/todos?userId={employee_id}'
-    response = requests.get(API_info)
-    if response.status_code != 200:
-        print("Error:", response.text)
-        return
-
-    data = response.json()
-    employee_name = data[0]['userId']  # Assuming 'userId' contains the employee's name
-    
-    completed_tasks = []
-    for task in data:
-        if task['completed']:
-            completed_tasks.append(task['title'])
-    
-    total_tasks = len(data)
-
-    print(f"Employee {employee_name} is done with tasks({len(completed_tasks)}/{total_tasks}):")
-    for task_title in completed_tasks:
-        print(f"    {task_title}")
-
-    CSV_file = f"{employee_id}.csv"
-    with open(CSV_file, 'w', newline='') as csv_file:
-        file_names = ['USER_ID', 'USERNAME', 'TASK_COMPLETE_STATUS', 'TASK_TITLE']
-        writer = csv.DictWriter(csv_file, fieldnames=file_names)
-
-        writer.writeheader()
-        for task in data:
-            writer.writerow({
-                'USER_ID': employee_id,
-                'USERNAME': task['userId'],
-                'TASK_COMPLETED_STATUS': task['completed'],
-                'TASK)_TITLE': task['title']
-            })
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print("Usage: python3 script.py <employee_id>")
+        print(f"UsageError: python3 {__file__} employee_id(int)")
         sys.exit(1)
 
-    employee_id = int(sys.argv[1])
-    fetch_employee_todo_list(employee_id)
+    API_URL = "https://jsonplaceholder.typicode.com"
+    EMPLOYEE_ID = sys.argv[1]
+
+    response = requests.get(
+        f"{API_URL}/users/{EMPLOYEE_ID}/todos",
+        params={"_expand": "user"}
+    )
+    data = response.json()
+
+    if not len(data):
+        print("RequestError:", 404)
+        sys.exit(1)
+
+    employee_name = data[0]["user"]["name"]
+    total_tasks = len(data)
+    done_tasks = [task for task in data if task["completed"]]
+    total_done_tasks = len(done_tasks)
+
+    csv_format = []
+
+    print(f"Employee {employee_name} is done with tasks"
+            f"({total_done_tasks}/{total_tasks}):")
+    for task in done_tasks:
+        print(f"\t {task['title']}")
+
+    with open('USER_ID.csv', 'w', newline='') as csv_file:
+        writer = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
+        writer.writerows(csv_format)
